2675b18391f1c2ba48cad134363401ee3c7b6e22
belato <sola.belator@gmail.com>
2025-05-05 17:38:12 +0300

    update json

--- src/game.js
+++ src/game.js
@@ -1,39 +1,24 @@
 import readlineSync from "readline-sync";
-
-const randomNumber = () => Math.round(Math.random() * 100);
+import { randomNum, getAnswer, wrongAnswer, gameEnd } from "./index.js";
 
 export default () => {
   console.log("Welcome to the Brain Games");
-  let userName = readlineSync.question("May I have your name? ");
 
-  console.log("Hello, " + userName + " !");
+  const playerName = readlineSync.question("May I have your name? ");
+
+  console.log("Hello, " + playerName + " !");
   console.log("Answer 'yes' if the number is even, otherwise answer 'no'");
 
   for (let i = 0; i < 3; i += 1) {
-    let getNum = randomNumber();
-    let answer = readlineSync.question("Question: " + getNum + " ", {
-      hideEchoBack: true,
-      mask: "",
-    });
-    console.log("Your answer: " + answer);
+    let getNum = randomNum();
+    let answer = getAnswer(getNum);
     let result = getNum % 2 === 0 ? "yes" : "no";
+
     if (result === answer) {
       console.log("Correct!");
     } else {
-      return console.log(
-        "'" +
-          answer +
-          "'" +
-          " is wrong answer ;(. Correct answer was " +
-          "'" +
-          result +
-          "'" +
-          "." +
-          "Let's try again, " +
-          userName +
-          "!"
-      );
+      return wrongAnswer(answer, result, playerName);
     }
+    gameEnd(playerName);
   }
-  console.log("Congratulations, " + userName + "!");
 };
