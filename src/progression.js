408812479b6e19b18ee3d6184e5fbd775b181c78
belato <sola.belator@gmail.com>
2025-05-08 18:16:54 +0300

    correct index value

--- src/progression.js
+++ src/progression.js
@@ -18,10 +18,10 @@ export default () => {
   task(playerName, taskValue);
 
   for (let i = 0; i < 3; i += 1) {
-    const index = randomNum(0, 9);
+    const arr = new Array();
+    const index = randomNum(1, 10);
     const start = randomNum(1, 100);
     const limit = 10 * index + start;
-    const arr = [];
     for (let i = start; i < limit; i += index) {
       arr.push(i);
     }
