f34bed0dff2974557381fed89c7909a47962bb22
belato <sola.belator@gmail.com>
2025-05-09 13:35:22 +0300

    fix even

--- src/games/even.js
+++ src/games/even.js
@@ -2,7 +2,7 @@ import readlineSync from "readline-sync";
 import { randomNum, getAnswer, wrongAnswer, gameEnd } from "../index.js";
 
 export default () => {
-  console.log("Welcome to the Brain Games");
+  console.log("Welcome to the Brain Games!");
 
   const playerName = readlineSync.question("May I have your name? ");
 
