a1a08a2a6ad9643ece0fae22cb88536524802a90
belato <sola.belator@gmail.com>
2025-05-12 23:47:52 +0300

    fix missing !

--- src/index.js
+++ src/index.js
@@ -14,59 +14,10 @@ export default (task, data) => {
     const answer = readlineSync.question('You answer: ')
     if (expectedValue !== answer) {
       console.log(`"${answer}" is wrong answer ;(. Correct answer was "${expectedValue}".`)
-      console.log(`Let's try again, ${userName}`)
+      console.log(`Let's try again, ${userName}!`)
       return
     }
     console.log('Correct!')
   }
   console.log(`Congratulations, ${userName}!`)
 }
-
-// const randomNum = (a, b) => _.random(a, b)
-
-// const welcome = () => console.log('Welcome to the Brain Games!')
-// const task = (playerName, task) => {
-//   console.log('Hello, ' + playerName + '!')
-//   console.log(task)
-// }
-
-// const getAnswer = (random) => {
-//   console.log('Question: ' + random)
-//   let answer = readlineSync.question('You answer: ')
-//   return answer
-// }
-
-// const wrongAnswer = (answer, result, userName) => {
-//   console.log(`"${answer}" is wrong answer ;(. Correct answer was "${result}".`)
-//   console.log(`Let's try again, ${userName}`)
-// }
-
-// const isPrime = (number) => {
-//   const result = []
-//   if (number < 2) {
-//     return false
-//   }
-//   for (let i = 2; i <= number; i += 1) {
-//     if (number % i === 0) {
-//       result.push(i)
-//     }
-//   }
-//   return result.length === 1
-// }
-
-// const getGcd = (a, b) => (a % b === 0 ? b : getGcd(b, a % b))
-
-// const gameEnd = (playerName) => {
-//   console.log(`Congratulations, ${playerName}!`)
-// }
-
-// export {
-//   randomNum,
-//   getAnswer,
-//   wrongAnswer,
-//   gameEnd,
-//   welcome,
-//   task,
-//   getGcd,
-//   isPrime,
-// }
